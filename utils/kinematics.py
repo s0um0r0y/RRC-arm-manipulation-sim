60eaf11ad39ce26bc6ff54e0593073d2949bd860
s0um0r0y <soumoroy09@gmail.com>
2026-04-26 16:23:34 +0530

    [FEAT]: added ee pose function

--- utils/kinematics.py
+++ utils/kinematics.py
@@ -1,3 +1,25 @@
 import numpy as np
 import mujoco
 
+def get_ee_pose(model: mujoco.MjModel, data: mujoco.MjData, site_name: str) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Computes the Forward Kinematics for a given site (End-Effector).
+    
+    Args:
+        model: MuJoCo model.
+        data: MuJoCo data (contains current state).
+        site_name: The name of the site defined in the XML (e.g., 'attachment_site').
+        
+    Returns:
+        pos: 3D position vector (x, y, z) of the site.
+        mat: 3x3 rotation matrix representing the site's orientation.
+    """
+    site_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_SITE, site_name)
+    if site_id == -1:
+        raise ValueError(f"Site '{site_name}' not found in the model.")
+    
+    # extract position and rotation matrix
+    pos = data.site_xpos[site_id].copy()
+    mat = data.size_xmat[site_id].reshape(3, 3).copy()
+    
+    return pos, mat
