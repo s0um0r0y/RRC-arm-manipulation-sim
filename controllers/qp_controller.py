71d957e4b3fbddf2ee30dd278ad3ca01d9b27098
s0um0r0y <soumoroy09@gmail.com>
2026-06-14 23:07:01 +0530

    [FEAT]: QP controller placeholder

---
+++ controllers/qp_controller.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cvxpy as cp 
+from scipy.spatial.transform import Rotation as R
+
+class QPImpedanceController:
+    def __init__(self) -> None:
+        pass
+        
+    def compute_torques():
+        pass
