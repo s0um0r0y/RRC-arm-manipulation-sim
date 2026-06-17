8a2bc02f53d1016223db455aaab0e99407c32531
s0um0r0y <soumoroy09@gmail.com>
2026-06-17 08:40:08 +0530

    [FEAT]: added the constraints and objectives

--- controllers/qp_controller.py
+++ controllers/qp_controller.py
@@ -1,3 +1,4 @@
+from cvxpy.constraints import constraint
 import numpy as np
 import cvxpy as cp 
 from scipy.spatial.transform import Rotation as R
@@ -9,6 +10,26 @@ class QPImpedanceController:
         self.Kd_pos = Kd_pos
         self.Kp_ori = Kp_ori
         self.Kd_ori = Kd_ori
+        
+        # CVXPY setup
+        self.ddq = cp,cp.Variable(self.nv)
+        self.J_param = cp.Parameter((6, self.nv))
+        self.xdd_cmd_param = cp.Parameter(6)
+        
+        # regularization weight to keep joint accelerations small
+        self.lam = 0.01
+        
+        # objective: minmize ||J * ddq - xdd_cmd||^2 + lambda * ||ddq||^2
+        cost = cp.sum_squares(self.J_param @ self.ddq - self.xdd_cmd_param) + self.lam * cp.sum_squares(self.ddq)
+        
+        # constraints : limit maximum joint accelerations for safety
+        # (e.g., -10 to 10 rad/s^2)
+        constraints = [
+            self.ddq >= -10.0,
+            self.ddq <= 10.0
+        ]
+        
+        self.prob = cp.Problem(cp.Minimize(cost), constraints)
         
     def compute_torques():
         pass
