6bc40ef9d0c03cefb71856769a055375356bd315
s0um0r0y <soumoroy09@gmail.com>
2026-06-18 10:27:53 +0530

    [FEAT]: compute torques

--- controllers/qp_controller.py
+++ controllers/qp_controller.py
@@ -31,5 +31,41 @@ class QPImpedanceController:
         
         self.prob = cp.Problem(cp.Minimize(cost), constraints)
         
-    def compute_torques():
-        pass
+    def compute_torques(
+        self, 
+        J: np.ndarray, M: np.ndarray, qfrc_bias: np.ndarray,
+        pos_curr: np.ndarray, mat_curr: np.ndarray, vel_curr: np.ndarray,
+        pos_des: np.ndarray, mat_des: np.ndarray, vel_des: np.ndarray
+    ) -> np.ndarray:
+        
+        # position error
+        error_pos = pos_des - pos_curr
+        error_vel_pos = vel_des[:3] - vel_curr[:3]
+        
+        # orientation error
+        R_c = R.from_matrix(mat_curr)
+        R_d = R.from_matrix(mat_des)
+        error_ori = (R_d * R_c.inv()).as_rotvec()
+        error_vel_ori = vel_des[3:] - vel_curr[3:]
+        
+        # commanded task space accelerations (impedance law)
+        acc_pos = self.Kp_pos @ error_pos + self.Kd_pos @ error_vel_pos
+        acc_ori = self.Kp_ori @ error_ori + self.Kd_ori @ error_vel_ori
+        xdd_cmd = np.concatenate([acc_pos, acc_ori])
+        
+        # solve the QP for optimal joint acceleration (ddq)
+        self.J_param.value = J
+        self.xdd_cmd_param.value = xdd_cmd
+        
+        try:
+            self.prob.solve(solver=cp.OSQP, warm_start=True)
+            ddq_opt = self.ddq.value
+            if ddq_opt is None:
+                raise ValueError("QP failed to find a solution.")
+        except Exception:
+            ddq_opt = np.zeros(self.nv)
+            
+        # Inverse dynamics: convert optimal accelerations into torques    
+        tau = M @ ddq_opt + qfrc_bias
+        
+        return tau
